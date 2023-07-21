c7350573ccbf96e13b6c643cd58d362c4407af5f
eric jamli <jamlietag@outlook.fr>
2023-07-21 13:41:23 +0200

    dernier commit du responsive

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -2,19 +2,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './404.css';
-import hommeperdu from "../../assets/images/hommeperdu.webp"
+  
  
-
 function Error404 () {
     return (
         <div className="p404">
             <div className="page404">
-            <h1>Oops!</h1>
-             <img src={hommeperdu} alt=" un homme perdu" srcSet="" />
-             <p>Désolé, la page demandée n'existe pas.</p>
+            <div className="container404">
+            <h1>404</h1>
+            </div>
+            
+             <p>Oups! La page que vous demandez n'existe pas.</p>
             </div>
             <Link to ="/" className="lin">
-                Retournez sur la page d'accueil
+                Retourner sur la page d'accueil
             </Link>
         </div>
     )
